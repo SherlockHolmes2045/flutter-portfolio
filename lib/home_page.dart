6c44c254da6366d322b6b1accc285151da37bb8e
Ivan Lemovou <lemovou@gmail.com>
2025-01-15 11:09:21 +0100

    feat: articles section

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/colors.dart';
 import 'package:portfolio/semi_circle.dart';
 import 'package:portfolio/social_network_widget.dart';
@@ -129,7 +131,8 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           Text(
                             'My goal is to write maintanable, clean \nand understandable code to process \ndevelopment was enjoyable.',
-                            style: TextStyle(fontFamily: 'OpenSans'),
+                            style:
+                                TextStyle(fontFamily: 'OpenSans', color: grey),
                           ),
                           Text('Developer',
                               style: Theme.of(context)
@@ -141,15 +144,148 @@ class _HomePageState extends State<HomePage> {
                                       letterSpacing: 3)),
                         ],
                       ),
-                      Row(
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 100, right: 100, top: 20, bottom: 40),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              spacing: 10,
+                              children: [
+                                SocialNetworkWidget(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.github,
+                                      color: white,
+                                      size: 12,
+                                    ),
+                                    text: 'Github'),
+                                SocialNetworkWidget(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.linkedinIn,
+                                      color: white,
+                                      size: 12,
+                                    ),
+                                    text: 'Linkedin'),
+                              ],
+                            ),
+                            SocialNetworkWidget(
+                                icon: FaIcon(
+                                  FontAwesomeIcons.telegram,
+                                  color: white,
+                                  size: 12,
+                                ),
+                                text: 'Telegram'),
+                            Row(
+                              spacing: 10,
+                              children: [
+                                SocialNetworkWidget(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.xTwitter,
+                                      color: white,
+                                      size: 12,
+                                    ),
+                                    text: 'Twitter'),
+                                SocialNetworkWidget(
+                                    icon: FaIcon(
+                                      FontAwesomeIcons.stackOverflow,
+                                      color: white,
+                                      size: 12,
+                                    ),
+                                    text: 'StackOverflow')
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Stack(
                         children: [
-                          SocialNetworkWidget(
-                              icon: Icon(
-                                Icons.cabin,
-                                size: 12,
-                                color: white,
-                              ),
-                              text: 'Github')
+                          FlutterCarousel(
+                            options: FlutterCarouselOptions(
+                              height: 200.0,
+                              showIndicator: true,
+                              slideIndicator: CircularSlideIndicator(),
+                            ),
+                            items: [1, 2, 3, 4, 5].map((i) {
+                              return Builder(
+                                builder: (BuildContext context) {
+                                  return Container(
+                                    width: 300,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Stack(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          child: Image.network(
+                                            width: 300,
+                                            height: 150,
+                                            'https://miro.medium.com/v2/resize:fit:1400/format:webp/0*HFY-Y_wR9JQNmxIY.png',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Container(
+                                              width: 200,
+                                              height: 150,
+                                              decoration: BoxDecoration(
+                                                  color: darkGrey
+                                                      .withOpacity(0.95),
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                          topRight:
+                                                              Radius.circular(
+                                                                  20),
+                                                          bottomRight:
+                                                              Radius.circular(
+                                                                  20))),
+                                              child: Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 16,
+                                                    right: 8.0,
+                                                    top: 8.0,
+                                                    bottom: 8.0),
+                                                child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.start,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      'The simple example is \nkafka + golang',
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          fontFamily:
+                                                              'OpenSans'),
+                                                    ),
+                                                    Text(
+                                                      'This article presents a simple way to implement a micro service architecture using Kafka, Golang and Docker.',
+                                                      style: TextStyle(
+                                                          color: grey,
+                                                          fontSize: 8,
+                                                          fontFamily:
+                                                              'OpenSans'),
+                                                    )
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                },
+                              );
+                            }).toList(),
+                          )
                         ],
                       )
                     ],
